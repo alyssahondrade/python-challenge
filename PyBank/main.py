6caa693d82b07b9c5fe7be29b86a869280e4e9ab
Alyssa H <alyssa_hondrade@icloud.com>
2023-08-01 13:53:06 +0800

    get values of greatest change and its month

--- PyBank/main.py
+++ PyBank/main.py
@@ -13,10 +13,13 @@ test_greatest_decrease = -1825558
 total_months = 0
 net_profit = 0
 profit_column = [] # hold the profit/losses values for comparison
+date_column = [] # hold the date values for finding greatest increase & decrease
 changes = [] # list to track changes in profit/losses
 average_change = 0 # calculated value using 'changes' list
 greatest_increase = 0
 greatest_decrease = 0
+month_increase = ""
+month_decrease = ""
 
 # Import budget_data.csv
 budget_csv = os.path.join("./PyBank/resources", "budget_data.csv")
@@ -29,29 +32,44 @@ with open(budget_csv) as csvfile:
     print(csv_header)
 
     for row in csvreader:
-        profit = row[1]
+        # Typecast (str -> int) at the start
+        profit = int(row[1])
         # Increment each month to total_months
         total_months += 1
 
-        # Increment net_profit, after typecasting to int
-        net_profit += int(profit)
+        # Increment net_profit
+        net_profit += profit
 
-        # Populate profit_column
+        # Populate profit_column and date_column
         profit_column.append(profit)
+        date_column.append(row[0])
 
 # Find the profit/losses changes
 for index, value in enumerate(profit_column):
+    profit = int(value)
     if (index > 0):
         change = int(profit_column[index]) - int(profit_column[index-1])
         changes.append(change)
 
+for index, value in enumerate(changes):
+    # 'changes' index is shifted left, must use [index + 1] to get correct month
+    if (value > greatest_increase):
+        greatest_increase = value
+        month_increase = date_column[index+1]
+
+    if (value < greatest_decrease):
+        greatest_decrease = value
+        month_decrease = date_column[index+1]
+
 average_change = round(mean(changes), 2)
-greatest_increase = max(changes)
-greatest_decrease = min(changes)
+# greatest_increase = max(changes)
+# greatest_decrease = min(changes)
 
 print(total_months)
 print(net_profit)
 #print(changes)
-print(greatest_increase)
-print(greatest_decrease)
+print(f"{greatest_increase} occurred on {month_increase}")
+print(f"{greatest_decrease} occurred on {month_decrease}")
 print(average_change)
+
+print(len(changes))
