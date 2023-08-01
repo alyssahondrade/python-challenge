817efd60f6a69d07e3a9e7eeeecfe933b7a79e40
Alyssa H <alyssa_hondrade@icloud.com>
2023-08-01 19:03:36 +0800

    calculate total number of votes each candidate won

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,6 +7,9 @@ candidates = []
 vote_percentage = []
 won_votes = []
 
+# Initialise variables - Helper
+candidate_column = []
+
 # Import election_data.csv
 election_csv = os.path.join("./PyPoll/resources", "election_data.csv")
 
@@ -17,10 +20,20 @@ with open(election_csv) as csvfile:
     csv_header = next(csvreader)
     
     for row in csvreader:
+        # Get the total number of votes cast
         total_votes += 1
 
+        # Get list of candidates who received votes
         if row[2] not in candidates:
             candidates.append(row[2])
+        
+        # Populate candidate_column
+        candidate_column.append(row[2])
+
+for candidate in candidates:
+    won_votes.append(candidate_column.count(candidate))
+    
 
 print(total_votes)
 print(candidates)
+print(won_votes)
